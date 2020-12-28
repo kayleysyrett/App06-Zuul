eeb03dfcd73e00ddf8175c565a4ef184846f7d4e
kayleysyrett <73078305+kayleysyrett@users.noreply.github.com>
2020-12-28 22:23:51 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# App06-Zuul
+ A test copy of the Zuul game using IJ
+ 
+ to be tried by:
+ Derek Peacock and Andrei Cruceru
+ 
+ 
---
+++ src/Main.java
@@ -0,0 +1,16 @@
+/**
+ * Based on the Collosal Cave Adventure game
+ */
+public class Main
+{
+    private static Game zuul;
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        zuul = new Game();
+    }
+}
